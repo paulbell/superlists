ea83ea3761f60c512c0a895c1793dd870a50e3e2
Paul Bell <paulb@naturalpower.com>
2014-08-22 16:51:52 +0100

    WIP for ch 7

--- lists/models.py
+++ lists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class List(models.Model):
-	pass
+	name = models.TextField(default='')
 
 class Item(models.Model):
 	text = models.TextField(default='')
